9825a0020a49aa1fe3ae5e90aeb97d3dd7be99f2
kitatora <mina3.ryu0728@gmail.com>
2020-06-01 22:04:13 +0900

    正解(要ブラッシュアップ)

---
+++ Paiza/paizad003.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+# 自分の得意な言語で
+# Let's チャレンジ！！
+
+input_line = input()
+i1 = int(input_line)*1
+i2 = int(input_line)*2
+i3 = int(input_line)*3
+i4 = int(input_line)*4
+i5 = int(input_line)*5
+i6 = int(input_line)*6
+i7 = int(input_line)*7
+i8 = int(input_line)*8
+i9 = int(input_line)*9
+
+print(i1, i2, i3, i4, i5, i6, i7, i8, i9)
