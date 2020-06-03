7c65f3424998f711db6a39185e510bf4adda585a
kitatora <mina3.ryu0728@gmail.com>
2020-06-03 21:10:16 +0900

    20200603

--- Paiza/paizad004.py
+++ Paiza/paizad004.py
@@ -2,10 +2,9 @@
 # 自分の得意な言語で
 # Let's チャレンジ！！
 
-import sys
 input_line = input()
-for input_line in sys.stdin.readlines():
-    print(",".join(input_line.rstrip()))
+for word in input_line.readlines():
+    print(",".join(word.rstrip()))
 
 #実行結果：期待する結果にならず
 #P,a,i,z,a
