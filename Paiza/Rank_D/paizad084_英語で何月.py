b23d2ac67ecd10e83e57ac9752c8fce784f0aa3f
kitatora <mina3.ryu0728@gmail.com>
2020-06-30 22:46:41 +0900

    20200630

---
+++ Paiza/Rank_D/paizad084_英語で何月.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+# 自分の得意な言語で
+# Let's チャレンジ！！
+month = {1:"January", 2: "Feburary", 3:"March", 4:"April", 5:"May", 6:"June", 
+7:"July", 8:"August", 9:"September", 10:"October", 11:"November", 12:"December"}
+M = int(input())
+print(month[M])
---
+++ Paiza/Rank_D/paizad143_制動距離の計算.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+# 自分の得意な言語で
+# Let's チャレンジ！！
+
+M, V, F = input().split()
+m = int(M)
+v = int(V)
+f = int(F)
+l = m*(v**2)/(2*f)
+print(int(l))
