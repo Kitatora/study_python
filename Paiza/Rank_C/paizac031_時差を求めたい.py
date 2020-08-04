1ecdae701d17ed960ec6d3041ce87a11e9a0d129
kitatora <mina3.ryu0728@gmail.com>
2020-08-04 10:14:30 +0900

    20200804

--- Paiza/Rank_C/paizac031_時差を求めたい.py
+++ Paiza/Rank_C/paizac031_時差を求めたい.py
@@ -2,19 +2,52 @@
 # 自分の得意な言語で
 # Let's チャレンジ！！import datetime
 
+# n = int(input())
+# li = []
+# for i in range(n):
+#     li.append(list(map(str, input().split())))
+# a, b = input().split()
+# b_time = datetime.datetime.strptime(b, "%H:%M")
+# # print(li)
+# print(b_time)
+# li2 = []
+# for i in range(1, n+1):
+#     li2.append(li[i-1][1])
+# print(li2)
+# li3 = list(map(int, li2))
+# for i in range(len(li3)):
+#     n_time = b_time + datetime.timedelta(li3[i])
+#     print(n_time)
+
+# coding: utf-8
+# 自分の得意な言語で
+# Let's チャレンジ！！
+
+import datetime as dt
+
 n = int(input())
 li = []
 for i in range(n):
     li.append(list(map(str, input().split())))
+
+print(li)
 a, b = input().split()
-b_time = datetime.datetime.strptime(b, "%H:%M")
-# print(li)
+print(a)
+print(b)
+# b_time = dt.datetime(b, "%H:%M")
+b_time = dt.datetime.strptime(b, "%H:%M").time
+# t1=datetime.strptime('09:00:00','%H:%M:%S').time()
 print(b_time)
-li2 = []
-for i in range(1, n+1):
-    li2.append(li[i-1][1])
-print(li2)
-li3 = list(map(int, li2))
-for i in range(len(li3)):
-    n_time = b_time + datetime.timedelta(li3[i])
-    print(n_time)
+# # print(li)
+# # print(b_time)
+# li2 = []
+# for i in range(1, n+1):
+#     li2.append(li[i-1][1])
+# print(li2)
+# li3 = list(map(int, li2)) 
+# for i in range(len(li3)):
+#     n_time = b_time + datetime.timedelta(hours = li3[i])
+#     print(n_time)
+    #時間と分を出力する方法を調べる
+    #時差の算出方法を再確認
+
