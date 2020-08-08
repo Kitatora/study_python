f7e16285693012dab96335c5c89c9bbbf435608d
kitatora <mina3.ryu0728@gmail.com>
2020-08-08 18:38:49 +0900

    20200808

--- Paiza/Rank_C/paizac031_時差を求めたい.py
+++ Paiza/Rank_C/paizac031_時差を求めたい.py
@@ -1,26 +1,5 @@
 # coding: utf-8
 # 自分の得意な言語で
-# Let's チャレンジ！！import datetime
-
-# n = int(input())
-# li = []
-# for i in range(n):
-#     li.append(list(map(str, input().split())))
-# a, b = input().split()
-# b_time = datetime.datetime.strptime(b, "%H:%M")
-# # print(li)
-# print(b_time)
-# li2 = []
-# for i in range(1, n+1):
-#     li2.append(li[i-1][1])
-# print(li2)
-# li3 = list(map(int, li2))
-# for i in range(len(li3)):
-#     n_time = b_time + datetime.timedelta(li3[i])
-#     print(n_time)
-
-# coding: utf-8
-# 自分の得意な言語で
 # Let's チャレンジ！！
 
 import datetime as dt
@@ -30,25 +9,23 @@ li = []
 for i in range(n):
     li.append(list(map(str, input().split())))
 
+a, b = input().split() #singapore 19:38
+b_time = dt.datetime.strptime(b, "%H:%M")
+# print(li)
+# print(i)
+for i in li:
+    # print(i)
+    if i[0] == a:
+        country = int(i[1])
+# print(country)
 
-
-a, b = input().split()
-print(a)
-print(b)
-# b_time = dt.datetime(b, "%H:%M")
-b_time = dt.datetime.strptime(b, "%H:%M").time
-# t1=datetime.strptime('09:00:00','%H:%M:%S').time()
-print(b_time)
-# # print(li)
-# # print(b_time)
-# li2 = []
-# for i in range(1, n+1):
-#     li2.append(li[i-1][1])
+li2 = []
+for i in li:
+    li2.append(int(i[1])-country)
 # print(li2)
-# li3 = list(map(int, li2)) 
-# for i in range(len(li3)):
-#     n_time = b_time + datetime.timedelta(hours = li3[i])
-#     print(n_time)
-    #時間と分を出力する方法を調べる
-    #時差の算出方法を再確認
+
+for i in range(len(li2)):
+    n_time = b_time + dt.timedelta(hours = li2[i])
+    str_MH = dt.datetime.strftime(n_time, "%H:%M")
+    print(str_MH)
 
